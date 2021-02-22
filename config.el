619908bc885b3f6fd5be4ac7efe56a0cd1136ea9
Augustin Thiercelin <augustin.thiercelin@outlook.com>
2021-02-22 10:27:39 +0100

    config: add org templates

--- config.el
+++ config.el
@@ -63,6 +63,55 @@
 
 (setq org-export-with-email t)
 
+;; Org capture templates
+(setq org-capture-templates
+      '(("t" "Personal todo" entry
+        (file+headline +org-capture-todo-file "Misc")
+        "* [ ] %?
+%i
+%a" :prepend t)
+       ("i" "Important todo" entry
+        (file+headline +org-capture-todo-file "Cours")
+        "* [ ] %?
+%i
+%a" :prepend t)
+       ("n" "Personal notes" entry
+        (file+headline +org-capture-notes-file "Inbox")
+        "* %u %?
+%i
+%a" :prepend t)
+       ("j" "Journal" entry
+        (file+olp+datetree +org-capture-journal-file)
+        "* %U %?
+%i
+%a" :prepend t)
+       ("p" "Templates for projects")
+       ("pt" "Project-local todo" entry
+        (file+headline +org-capture-project-todo-file "Inbox")
+        "* TODO %?
+%i
+%a" :prepend t)
+       ("pn" "Project-local notes" entry
+        (file+headline +org-capture-project-notes-file "Inbox")
+        "* %U %?
+%i
+%a" :prepend t)
+       ("pc" "Project-local changelog" entry
+        (file+headline +org-capture-project-changelog-file "Unreleased")
+        "* %U %?
+%i
+%a" :prepend t)
+       ("o" "Centralized templates for projects")
+       ("ot" "Project todo" entry #'+org-capture-central-project-todo-file "* TODO %?
+ %i
+ %a" :heading "Tasks" :prepend nil)
+       ("on" "Project notes" entry #'+org-capture-central-project-notes-file "* %U %?
+ %i
+ %a" :heading "Notes" :prepend t)
+       ("oc" "Project changelog" entry #'+org-capture-central-project-changelog-file "* %U %?
+ %i
+ %a" :heading "Changelog" :prepend t)))
+
 ;; Custom dailies templates
 (setq org-roam-dailies-capture-templates
       '(("c" "cours" entry
